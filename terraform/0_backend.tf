7281b864c961516235d786f0e986c90c9b4c05a3
Carlos Solis Salazar <carlos@soysoliscarlos.com>
2026-06-19 12:47:07 -0500

    Add Terraform configuration files for Azure resources and variables

---
+++ terraform/0_backend.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.15.0"
+
+  backend "azurerm" {
+    resource_group_name  = "rg-utpdavid2026-GEN-4j"
+    storage_account_name = "stgutpdavid2026gen4j"
+    container_name       = "tfstate-user1"
+    key                  = "terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~> 2.7"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7"
+    }
+  }
+}
